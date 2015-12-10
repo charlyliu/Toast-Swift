b7113458b898b4093e557080d33329cca9f55be0
Charly Liu Chou <charly.liu@schibsted.es>
2015-12-10 15:37:07 +0100

    Add Toast Swift Framework

---
+++ ToastSwiftFramework/ToastSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  ToastSwiftFramework.h
+//  ToastSwiftFramework
+//
+//  Created by Charly Liu Chou on 12/10/15.
+//  Copyright © 2015 Charles Scalesse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToastSwiftFramework.
+FOUNDATION_EXPORT double ToastSwiftFrameworkVersionNumber;
+
+//! Project version string for ToastSwiftFramework.
+FOUNDATION_EXPORT const unsigned char ToastSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastSwiftFramework/PublicHeader.h>
+
+
